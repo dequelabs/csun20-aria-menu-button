a2787edb0a69abe50fe34162110f2f7c5a431478
Harris Schneiderman <schne324@gmail.com>
2020-02-23 21:15:34 -0800

    :sparkle: unit tests :heavy_check_mark:

--- demo/Home.js
+++ demo/Home.js
@@ -1,25 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'cauldron-react'
+import MenuButton from '../'
+import './Home.css'
 
-const Home = () => (
-  <div className="Wrap">
-    <h1 id="main-heading">Home</h1>
-    <hr />
-    <p>
-      A Mango Beer behind the freight train ruminates, or
-      the blood clot pours freezing cold booze on a bull
-      ice. Sometimes a psychotic Corona Extra panics, but a
-      colt 45 near a Bacardi Silver always steals women from
-      a Bacardi Silver inside the Amarillo Pale Ale! The
-      polar bear beer for the line dancer plays pinochle
-      with the Imperial Stout around a Guiness. Most people
-      believe that a Left Hand Milk Stout usually graduates
-      from a twisted Hops Alligator Ale, but they need to
-      remember how accidentally a hypnotic spudgun gets
-      stinking drunk. Most people believe that a bud dry
-      conquers another discusting keg, but they need to
-      remember how barely a black velvet beams with joy.
-    </p>
-  </div>
-)
+const Home = () => {
+  const [actionLog, setActionLog] = useState([])
+  // NOTE: disabling below rule because it is vital that
+  // the scrollable action log region has tabIndex={0} so
+  // keyboard users are able to scroll the log with arrow
+  // keys
+  /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+  return (
+    <div className="Wrap">
+      <h1 id="main-heading">Home</h1>
+      <hr />
+      <p>
+        A Mango Beer behind the freight train ruminates, or
+        the blood clot pours freezing cold booze on a bull
+        ice. Sometimes a psychotic Corona Extra panics, but
+        a colt 45 near a Bacardi Silver always steals women
+        from a Bacardi Silver inside the Amarillo Pale Ale!
+        The polar bear beer for the line dancer plays
+        pinochle with the Imperial Stout around a Guiness.
+        Most people believe that a Left Hand Milk Stout
+        usually graduates from a twisted Hops Alligator Ale,
+        but they need to remember how accidentally a
+        hypnotic spudgun gets stinking drunk. Most people
+        believe that a bud dry conquers another discusting
+        keg, but they need to remember how barely a black
+        velvet beams with joy.
+      </p>
+      <MenuButton
+        buttonContent="Actions"
+        items={[
+          { children: <span>action 1</span> },
+          { children: <span>action 2</span> },
+          { children: <span>action 3</span> }
+        ]}
+        onSelection={el =>
+          setActionLog([...actionLog, el.innerText])
+        }
+      />
+      <h2 id="action-log-heading">Action log</h2>
+      <div
+        role="log"
+        tabIndex={0}
+        aria-labelledby="action-log-heading"
+      >
+        <ul>
+          {actionLog.map((item, i) => (
+            <li key={`${item}${i}`}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <Button
+          variant="secondary"
+          onClick={() => setActionLog([])}
+          className="Clear"
+        >
+          Clear log
+        </Button>
+      </div>
+      <div className="Links">
+        <Link to="/contact" className="dqpl-link">
+          Request a free quote today!
+        </Link>
+        <Link to="/about" className="dqpl-link">
+          Learn more about us
+        </Link>
+      </div>
+    </div>
+  )
+  /* eslint-enable jsx-a11y/no-noninteractive-tabindex */
+}
 
 export default Home
--- index.test.js
+++ index.test.js
@@ -1,1 +1,314 @@
-test.todo('write tests!')
+import React from 'react'
+import { mount } from 'enzyme'
+import MenuButton from './'
+
+const mountWrapper = (overrides = {}) =>
+  mount(
+    <MenuButton
+      buttonContent="Actions"
+      items={[
+        { children: <span>action 1</span> },
+        { children: <span>action 2</span> },
+        { children: <span>action 3</span> }
+      ]}
+      onSelection={() => {}}
+      {...overrides}
+    />
+  )
+
+describe('menu button trigger', () => {
+  describe('keyboard interaction', () => {
+    describe('down arrow', () => {
+      test('Opens menu and moves focus to first menuitem', () => {
+        const wrapper = mountWrapper()
+        wrapper.find('button').simulate('keydown', {
+          key: 'ArrowDown'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(0)
+            .getDOMNode()
+        )
+      })
+    })
+
+    describe('up arrow', () => {
+      test('Opens menu and moves focus to last menuitem', () => {
+        const wrapper = mountWrapper()
+        wrapper.find('button').simulate('keydown', {
+          key: 'ArrowUp'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(2)
+            .getDOMNode()
+        )
+      })
+    })
+  })
+
+  describe('roles/states/properties', () => {
+    test('The element that opens the menu has role button.', () => {
+      const wrapper = mountWrapper()
+      expect(
+        wrapper.find('button').exists() ||
+          wrapper.find('[role=button]').exists()
+      ).toBe(true)
+    })
+
+    test('The element with role button has aria-haspopup set to either menu or true.', () => {
+      const wrapper = mountWrapper()
+      const button = wrapper.find('button')
+      expect(
+        button.is('[aria-haspopup="true"]') ||
+          button.is('[aria-haspopup="menu"]')
+      ).toBe(true)
+    })
+
+    test('When the menu is displayed, the element with role button has aria-expanded set to true', () => {
+      const wrapper = mountWrapper()
+      const button = wrapper.find('button')
+      button.simulate('click')
+      const value = button
+        .getDOMNode()
+        .getAttribute('aria-expanded')
+      expect(value).toBe('true')
+    })
+
+    test('When the menu is not displayed, the element with role button has aria-expanded set to false', () => {
+      const wrapper = mountWrapper()
+      const button = wrapper.find('button')
+      const value = button
+        .getDOMNode()
+        .getAttribute('aria-expanded')
+      expect(value).toBe('false')
+    })
+
+    test('button has a value specified for aria-controls that refers to the element with role menu.', () => {
+      const wrapper = mountWrapper()
+      const button = wrapper.find('button').getDOMNode()
+      const menu = wrapper
+        .find('[role="menu"]')
+        .getDOMNode()
+
+      expect(button.getAttribute('aria-controls')).toBe(
+        menu.id
+      )
+    })
+  })
+})
+
+describe('menu button menu', () => {
+  describe('keyboard interaction', () => {
+    describe('Enter', () => {
+      test('Activates the menu item / Closes the menu. / Sets focus on the menu button', () => {
+        const onSelection = jest.fn()
+        const wrapper = mountWrapper({
+          onSelection
+        })
+        const button = wrapper.find('button').getDOMNode()
+        wrapper.find('button').simulate('click') // open the menu
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'Enter',
+          target: wrapper
+            .find('[role="menuitem"]')
+            .at(0)
+            .getDOMNode()
+        })
+        expect(onSelection).toHaveBeenCalled()
+        expect(button.getAttribute('aria-expanded')).toBe(
+          'false'
+        )
+        expect(document.activeElement).toBe(button)
+      })
+    })
+
+    describe('Escape', () => {
+      test('Closes the menu / Sets focus to the menu button', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'Escape'
+        })
+        expect(
+          button.getDOMNode().getAttribute('aria-expanded')
+        ).toBe('false')
+        expect(document.activeElement).toBe(
+          button.getDOMNode()
+        )
+      })
+    })
+
+    describe('Up arrow', () => {
+      test('Moves focus to the previous menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.setState({
+          focusIndex: 1
+        })
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'ArrowUp'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(0)
+            .getDOMNode()
+        )
+      })
+
+      test('If focus is on the first menu item, moves focus to the last menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.setState({
+          focusIndex: 0
+        })
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'ArrowUp'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(2)
+            .getDOMNode()
+        )
+      })
+    })
+
+    describe('Down arrow', () => {
+      test('Moves focus to the next menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.setState({
+          focusIndex: 0
+        })
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'ArrowDown'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(1)
+            .getDOMNode()
+        )
+      })
+
+      test('If focus is on the last menu item, moves focus to the first menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.setState({
+          focusIndex: 2
+        })
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'ArrowDown'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(0)
+            .getDOMNode()
+        )
+      })
+    })
+
+    describe('Home', () => {
+      test('Moves focus to the first menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'ArrowDown'
+        })
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'Home'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(0)
+            .getDOMNode()
+        )
+      })
+    })
+
+    describe('End', () => {
+      test('Moves focus to the first menu item', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'End'
+        })
+        expect(document.activeElement).toBe(
+          wrapper
+            .find('[role="menuitem"]')
+            .at(2)
+            .getDOMNode()
+        )
+      })
+    })
+
+    describe('Tab', () => {
+      test('Closes the menu', () => {
+        const wrapper = mountWrapper()
+        const button = wrapper.find('button')
+        button.simulate('click') // open the menu
+        wrapper.find('[role="menu"]').simulate('keydown', {
+          key: 'Tab'
+        })
+        expect(
+          button.getDOMNode().getAttribute('aria-expanded')
+        ).toBe('false')
+      })
+    })
+  })
+
+  describe('roles/states/properties', () => {
+    test('has role="menu"', () => {
+      expect(
+        mountWrapper()
+          .find('[role="menu"]')
+          .exists()
+      ).toBe(true)
+    })
+
+    test('The menu is labeled by the menu button.', () => {
+      const wrapper = mountWrapper()
+      const button = wrapper.find('button').getDOMNode()
+      const menu = wrapper
+        .find('[role="menu"]')
+        .getDOMNode()
+
+      expect(menu.getAttribute('aria-labelledby')).toBe(
+        button.id
+      )
+    })
+
+    test('children have role="menuitem"', () => {
+      mountWrapper()
+        .find('[role="menu"]')
+        .children()
+        .forEach(child => {
+          expect(
+            child.getDOMNode().getAttribute('role')
+          ).toBe('menuitem')
+        })
+    })
+
+    test('children have tabIndex={-1}', () => {
+      mountWrapper()
+        .find('[role="menu"]')
+        .children()
+        .forEach(child => {
+          expect(child.getDOMNode().tabIndex).toBe(-1)
+        })
+    })
+  })
+})
